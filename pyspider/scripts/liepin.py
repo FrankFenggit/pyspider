c251c83f6c8ad929130d54f888a204955673cc80
fengge <pjf1226@163.com>
2020-06-14 16:54:35 +0800

    同步更新到liepin

--- pyspider/scripts/liepin.py
+++ pyspider/scripts/liepin.py
@@ -5,6 +5,7 @@
 
 from pyspider.libs.base_handler import *
 import json
+import sqlite3
 
 import logging
 import logging.config
@@ -29,14 +30,45 @@ class Handler(BaseHandler):
             if not url or (url in self.baseUrl):
                 continue
             self.baseUrl.append(url)
-
+    def cleardb(self,dbpath,dbtbl_name):  # delete table 有时候无效 ，采用drop table
+        mylogger.info(sys._getframe().f_code.co_name)
+        try:
+            conn = sqlite3.connect(dbpath)
+            c = conn.cursor()
+            # 获取建表语句
+            sql_create = "select sql from sqlite_master where type='table' and  tbl_name='%s'"%(dbtbl_name)
+            cursor = c.execute(sql_create)
+            sql_create=""
+            for row in cursor:
+                sql_create=row[0]
+                break
+            # drop table
+            if not sql_create.isspace():
+                sql_drop="drop table %s"%(dbtbl_name)
+                cursor=c.execute(sql_drop)
+                cursor = c.execute(sql_create)
+                conn.commit()
+                mylogger.info("cleardb [%s:%s] %s succeful" % (dbpath, dbtbl_name,sql_drop))
+            else:
+                mylogger.error("[sql_create:isspace]")
+        except:
+            mylogger.error("cleardb [%s:%s] failed"%(dbpath,dbtbl_name))
+        finally:
+            cursor.close()
+            conn.close()
     def __init__(self):
         # 搜索URL基线
         self.baseUrl = []
 
     @every(minutes=1 * 60)
     def on_start(self):
-        mylogger.info("call on_start")
+        mylogger.info(sys._getframe().f_code.co_name)\
+        # 清空数据库 重新启动爬虫任务
+        dbtbl_name="resultdb_"+self.project_name
+        self.cleardb('./data/result.db',dbtbl_name);
+        dbtbl_name = "taskdb_" + self.project_name
+        self.cleardb('./data/task.db',dbtbl_name);
+        # 加载配置
         self.load_conf();
         for url in self.baseUrl:
             mylogger.info(url)
@@ -44,7 +76,7 @@ class Handler(BaseHandler):
 
     @config(age=0.5 * 60 * 60)
     def index_page(self, response):
-        mylogger.info("call index_page")
+        mylogger.info(sys._getframe().f_code.co_name)
         # 职位列表跳转
         for each in response.doc('div.job-info > h3 > a').items():
             mylogger.info("职位列表url: " + each.attr.href)
@@ -57,8 +89,8 @@ class Handler(BaseHandler):
 
     @config(priority=2)
     def detail_page(self, response):
-        mylogger.info("call detail_page")
-        result = {
+        mylogger.info(sys._getframe().f_code.co_name)
+        return {
             "url": response.url,
             "proj_name": self.project_name,
             "job_title": response.doc('h1').text(),
